312226c7b0a124064e48327ea25cc4725dc0f742
Brandon Vargas <andres.vpineros@gmail.com>
2025-04-23 08:07:56 -0500

    Integrate react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-function App() {
+const App = () => {
   return (
     <div className="container">
       <h1>Gaming Vault</h1>
     </div>
   );
-}
+};
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import App from "./App.tsx";
+import { BrowserRouter, Routes, Route } from "react-router";
+import HomePage from "./pages/home/HomePage.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/home" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
